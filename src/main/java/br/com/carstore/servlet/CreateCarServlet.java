718989ba022643336eb5e8c8dc4b2ddb6f892edb
dianalinaemidio <dianalina1203@gmail.com>
2024-03-27 21:01:54 -0300

    Atualizando projeto

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDAO;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +16,16 @@ public class CreateCarServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+
         String carName = request.getParameter("carName");
-        System.out.println("O nome do carro é: " + carName);
+
+        Car car = new Car();
+
+        car.setName(carName);
+
+        CarDAO carDao = new CarDAO();
+
+        carDao.createCar(car);
 
         request.getRequestDispatcher("index.html").forward(request,resp);
 
